761d5a6bdbcd601791248504fe16bfbb40ce4c5d
zjzdr <88304460+zjzdr@users.noreply.github.com>
2023-09-11 21:15:51 +0800

    Update mcr.cpp
    change subscripts

--- mcr.cpp
+++ mcr.cpp
@@ -31,7 +31,7 @@ int  main(){
     std::cout << "Player 1: ";
    else
     std::cout << "Player 2: ";
-   std::cout << "Which cell to mark? i:[1..3], j:[1..3]: "; 
+   std::cout << "Which cell to mark? i:[0..2], j:[0..2]: "; 
    std::cin >> i >> j;
    if (turn == false)
       game[i][j] = 'X';
